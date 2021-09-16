c0e196c1041d14d0762a39e6c5ac53d73677c798
Joseph D. Galloway II <josephgalloway520@gmail.com>
2021-09-15 20:10:14 -0700

    Almost done with problem II

--- Homework-II_joseph_galloway/homework_II.py
+++ Homework-II_joseph_galloway/homework_II.py
@@ -6,6 +6,7 @@
 import numpy as np
 import sympy as sp
 from sympy import symbols, Eq, solve
+from numpy import linalg as la
 
 from mpl_toolkits import mplot3d  # Plot function to verify answers
 import matplotlib.pyplot as plt
@@ -139,7 +140,46 @@ def problem_I():
 
 # Problem II
 def gradient_descent():
-    pass
+    x, y = sp.symbols('x y')
+    eps = 1e-3  # Termination criterion
+    k = 0  # Counter
+
+    # Function comes from the following equation:
+    # min. (1 - 2x - 3y + 1)^2 + x^2 + (y - 1)^2
+    # where x is x2
+    # and y is x3
+    # x1 is solved at the end when x2 and x3 are solved
+    function = (1 - 2*x - 3*y + 1)**2 + x**2 + (y - 1)**2
+    def partial_x(x, y): return 10*x + 12*y - 8
+    def partial_y(x, y): return 12*x + 20*y - 14
+
+    solution_x = [0]  # Initial guess x
+    solution_y = [0]  # Initial guess y
+    # Calculate gradient using initial guesses to calculate initial error
+    gradient = [partial_x(solution_x[0], solution_y[0]), partial_y(solution_x[0], solution_y[0])]
+
+    # Calculate the norm of the 2D gradient at the initial guesses
+    error = la.norm(gradient)
+
+
+    a = 0.05
+    while error >= eps:
+        # Calculate next step using gradient descent algorithm
+        solution_x.append(solution_x[k] - a*partial_x(solution_x[k], solution_y[k]))
+        solution_y.append(solution_y[k] - a*partial_y(solution_x[k], solution_y[k]))
+
+        # Update gradient norm and calculate new error using new x,y position
+        gradient = [partial_x(solution_x[k], solution_y[k]), partial_y(solution_x[k], solution_y[k])]
+        error = la.norm(gradient)
+        k += 1  # Increase counter
+
+    # Solution for x1 now that x2 and x3 are solved for
+    solution_x1 = 1 - 2*solution_x[k] - 3*solution_y[k]
+
+    soln = [solution_x1, solution_x[k], solution_y[k]]
+    print(soln)
+    # print(k)
+    # This solution matches what I calculated in part I of this problem.
 
 
 # Newton algorithm
@@ -148,9 +188,9 @@ def newton():
 
 
 def problem_II():
-    pass
     print("Problem II")
-
+    gradient_descent()
+    newton()
     print("____________________")
 
 
--- Homework-II_joseph_galloway/test_II.py
+++ Homework-II_joseph_galloway/test_II.py
@@ -1,23 +1,44 @@
 import sympy as sym
-
+from numpy import linalg as la
+import numpy as np
 
 x, y = sym.symbols('x y')
 eps = 1e-3  # Termination criterion
 k = 0  # Counter
 
-function = (1 - x)**2 + y**2
-# partial_x = sym.diff(function, x)
-# partial_y = sym.diff(function, y)
+# Function comes from the following equation:
+# min. (1 - 2x - 3y + 1)^2 + x^2 + (y - 1)^2
+# where x is x2
+# and y is x3
+# x1 is solved at the end when x2 and x3 are solved
+function = (1 - 2*x - 3*y + 1)**2 + x**2 + (y - 1)**2
+def partial_x(x, y): return 10*x + 12*y - 8
+def partial_y(x, y): return 12*x + 20*y - 14
+
+solution_x = [5]  # Initial guess x
+solution_y = [-7]  # Initial guess y
+# Calculate gradient using initial guesses to calculate initial error
+gradient = [partial_x(solution_x[0], solution_y[0]), partial_y(solution_x[0], solution_y[0])]
+
+# Calculate the norm of the 2D gradient at the initial guesses
+error = la.norm(gradient)
 
 
-def partial_x(x): return 2*(1 - x)
-def partial_y(y): return 2*y
+a = 0.05
+while error >= eps:
+    # Calculate next step using gradient descent algorithm
+    solution_x.append(solution_x[k] - a*partial_x(solution_x[k], solution_y[k]))
+    solution_y.append(solution_y[k] - a*partial_y(solution_x[k], solution_y[k]))
 
+    # Update gradient norm and calculate new error using new x,y position
+    gradient = [partial_x(solution_x[k], solution_y[k]), partial_y(solution_x[k], solution_y[k])]
+    error = la.norm(gradient)
+    k += 1  # Increase counter
 
-initial_guess = [0, 0]  # Initial guess for x and y
-solution_x = [initial_guess[0]]
-solution_y = [initial_guess[1]]
+# Solution for x1 now that x2 and x3 are solved for
+solution_x1 = 1 - 2*solution_x[k] - 3*solution_y[k]
 
-a = 0.01
-
-# while
+soln = [solution_x1, solution_x[k], solution_y[k]]
+print(soln)
+# print(k)
+# This solution matches what I calculated in part I of this problem.
