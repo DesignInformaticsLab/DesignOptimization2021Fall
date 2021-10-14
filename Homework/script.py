2e15fdb99184f62487e0044417ff1a6882a9b041
rahulrathnakumar <rahulrathnakumar.ae@gmail.com>
2021-10-14 14:42:00 -0700

    corrected ground truth objective function output in problem 2

--- Homework/script.py
+++ Homework/script.py
@@ -1,60 +1,72 @@
-# Problem 1 : Least squares fitting for vapor-liquid equilibrium
-import torch
+# Problem 2: Bayesian Optimization
+import sklearn.gaussian_process as gp
 import numpy as np
+from scipy.stats import norm
+from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-SMOOTH = 1e-5
+from mpl_toolkits import mplot3d
 
 
-def model(X, A, p_sat):
-    # Question : Temporary variables and the computational graph ...
-    k1 = A[0]*(A[1]*X[1]/(A[0]*X[0] + A[1]*X[1]))
-    k2 = A[1]*(A[0]*X[0]/(A[0]*X[0] + A[1]*X[1]))
-    t1 = X[0]*torch.exp((k1**2))*p_sat[0]
-    t2 = X[1]*torch.exp((k2**2))*p_sat[1]
-    return t1 + t2
+def objective(x):
+    '''
+    x : 2x1 vector of a sample in the domain of interest
+    mean : scalar mean for iid noise term
+    var : scalar variance for iid noise term
+    '''
+    return ((4 - (2.1*(x[0]**2)) + ((x[0]**4)/3))*(x[0]**2)) + (x[0]*x[1]) + ((-4 + 4*x[1]**2)*x[1]**2)
 
 
-# Define the variables to be optimized over
-A = torch.tensor([1.9,1.6], requires_grad = True, dtype = torch.float64)
-# Data
-X = np.asarray([np.arange(0,1.1,0.1), 1-np.arange(0,1.1,0.1)])
-X = torch.from_numpy(X)
-p = torch.tensor([28.1, 34.4, 36.7, 36.9, 36.8, 36.7, 36.5, 35.4, 32.9, 27.8, 17.5], requires_grad =  False, dtype = torch.float64)
 
-# Constants
-# a = torch.tensor([[8.07131, 1730.63, 233.426],[7.43155, 1554.679, 240.337]], requires_grad= False)
-# T = 20
-# p_sat_water = torch.tensor(a[0][0] - (a[0][1]/(T + a[0][2])))
-# p_sat_dioxane = torch.tensor(a[1][0] - (a[1][1]/(T + a[1][2])))
-p_sat = torch.tensor([1.2424, 1.4598], requires_grad = False, dtype = torch.float64)
-
-
-def phi(alpha,p, A):
-    return np.sum((p - model(X, A, p_sat)).detach().numpy()**2) - alpha*0.8*np.dot(A.grad, A.grad)
-
-def line_search(X, p, A, p_sat):
-    alpha = 0.001  # initialize step size
-    while phi(alpha, p, A)<np.sum((p - model(X, A-alpha*A.grad, p_sat)).detach().numpy()**2):  # if f(x+a*d)>phi(a) then backtrack. d is the search direction
-        alpha = 0.5*alpha
-    return alpha
-n_iter = 100
-for i in range(n_iter):
-    # Define the loss function
-    loss = torch.sum((p - model(X, A, p_sat))**2)
-    loss.backward()
-    alpha = line_search(X = X, p = p, A = A, p_sat = p_sat)
-    print("Alpha:", alpha, "Loss:", loss.data.numpy(), "Gradient:", A.grad.numpy(), "Parameter:", A.data.numpy())
-
-    with torch.no_grad():
-        A -= alpha * A.grad
-        if np.linalg.norm(A.grad) < 0.1:
-            break
-        A.grad.zero_()
-p_final = model(X, A, p_sat)
-print("Model: ", p_final)
-print("Truth: ", p)
-plt.plot(p_final.detach().numpy())
-plt.plot(p.detach().numpy())
+def expected_improvement(X_, X, model, k = 0.01):
+    Y_mu, Y_sigma = model.predict(X, return_std = True)
+    Y__= model.predict(X_)
+    
+    # Find the best value of predicted f(x) among the old samples
+    with np.errstate(divide='ignore'):
+        y_best = np.max(Y_sigma)
+        Z = (Y_mu - y_best - k)/Y_sigma
+        ei = ((Y_mu - y_best - k)*norm.cdf(Z)) + (Y_sigma*norm.pdf(Z))
+        ei[Y_sigma == 0.0] = 0.0
+    return X_[np.argmax(ei)]
 
 
+# optimize the expected improvement
+# EI function is highly non convex
+x1 = np.random.uniform(-3,3,10)
+x2 = np.random.uniform(-2,2,10)
+X = np.asarray([[x1],[x2]]).transpose().squeeze(1)
+Y = np.asarray([objective(x) for x in X])
+
+kernel = gp.kernels.Matern()
+model = gp.GaussianProcessRegressor(kernel=kernel,
+                                    alpha=1e-4,
+                                    n_restarts_optimizer=0,
+                                    normalize_y=True)
+n_iters = 20
+k = 0.01
+for i in range(n_iters):
+    print(i)
+    model.fit(X,Y)
+    # Select next point using expected improvement
+    x1 = np.random.uniform(-3,3,1000)
+    x2 = np.random.uniform(-2,2,1000)
+    X_ = np.asarray([[x1],[x2]]).transpose().squeeze(1)
+    x_next = expected_improvement(X_ = X_, X = X, model = model)
+    y_next = objective(x_next)
+    X = np.vstack((X, x_next))
+    Y = np.append(Y, y_next)
+
+x1_bnds = np.linspace(-3,3, 100)
+x2_bnds = np.linspace(-2, 2, 100)
+x_pts, y_pts= np.meshgrid(x1_bnds, x2_bnds)
+pts = np.asarray([x_pts,y_pts]).transpose()
+z__ = np.asarray([[objective(pt) for pt in pt_set] for pt_set in pts])
+
+
+
+fig = plt.figure(figsize = (10, 7))
+ax = plt.axes(projection ="3d")
+ax.scatter3D(X[:,0], X[:,1], Y, color = "green")
+ax.plot_surface(x_pts, y_pts, z__, color='gray', alpha = 0.5)
+plt.show()
